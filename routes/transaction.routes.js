4c0bd5f446ec7d0f7113b172070c1cad1d9b8f47
derryderajat <derryd.derajat@gmail.com>
2023-10-29 21:59:39 +0700

    Unroute endpoint POST /transactions

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const {
-  createTransaction,
   fetchTransactions,
   fetchTransactionById,
 } = require("../controller/transactions.controller");
-const {
-  isAmountPositive,
-  validateBankAccount,
-} = require("../middleware/middleware");
 
 const router = express.Router();
 
 router.get("/transactions", fetchTransactions);
 router.get("/transactions/:id", fetchTransactionById);
-router.post(
-  "/transactions",
-  [isAmountPositive, validateBankAccount],
-  createTransaction
-);
 
 module.exports = router;
